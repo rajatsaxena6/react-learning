9d09ff4b498fe18b177af9779d0df89cf06c5be7
rajat <rajatsaxena120@gmail.com>
2019-01-27 13:26:23 +0530

    person component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Person } from "./Person/Person";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 30 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
+  switchHandler = () => {
+    console.log("switch button clicked");
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React1
-          </a>
-        </header>
+        <h1>I am a React App.</h1>
+        <button onClick={this.switchHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
       </div>
     );
   }
---
+++ src/Person/Person.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Person = props => {
+  return (
+    <p>
+      Hello my name is {props.name} and my age is {props.age}
+    </p>
+  );
+};
